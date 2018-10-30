4883e317b17953fe43a7bd238d433bdf530ae4ae
Hoang <hoang@admins-MacBook-Pro.local>
2018-10-30 10:40:59 -0700

    add

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/users',users);
 app.use('/doctor',patients);
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
   })
 
 
